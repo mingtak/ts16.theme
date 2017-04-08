8217d3453f6dfafdc009395716442bce44dd63ca
Andy Fang <andy@mingtak.com.tw>
2017-04-08 15:48:44 +0000

    0408-1

---
+++ src/ts16/theme/browser/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone import api
+
+
+class Ts15Macro(BrowserView):
+    """  """
