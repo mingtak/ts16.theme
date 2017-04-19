31ca6c33303d75394c63312942598def2555beaa
Andy Fang <andy@mingtak.com.tw>
2017-04-19 19:51:27 +0800

    update 0419-6

--- src/ts16/theme/theme/js/script.js
+++ src/ts16/theme/theme/js/script.js
@@ -4,7 +4,7 @@ function loginToPoll(){
               <form action="to_vote"> \
                <input id="useremail" type="email" name="email"> \
                <input style="display:none" id="voteItems" type="text" name="voteItems" value="' + vm.vote + '"> \
-               <input type="submit" name="submit" class="btn bt-sm"> \
+               <input type="submit" id="vote-submit" name="submit" class="btn bt-sm"> \
               </form>'
 }
 
@@ -25,7 +25,10 @@ var vm = new Vue({
   data: {
     vote: [],
     voteMessage: 'hello world',
-    alert_1: '<h4>請選擇3個項目</h4>',
+    alert_1: '<h4>請選擇3個項目<a onclick="parent.$.colorbox.close(); return false;" \
+              class="btn btn-warning btn-s" style="margin-left:20px">返回投票</a></h4>',
+    alert_2: '<h4>請勾選確認「我已詳細閱讀並同意投票辦法」以完成投票.<a onclick="parent.$.colorbox.close(); return false;" \
+              class="btn btn-warning btn-s" style="margin-left:20px">返回投票</a></h4>',
     alert_final: '<h4>投標完成，感謝您的投票</h4>',
   },
 
@@ -42,7 +45,7 @@ var vm = new Vue({
             return;
         };
         if( vm.vote.length >= 3 ){
-            alert('只能選3個');
+            alert('只能投3票');
             return;
         };
         vm.vote.push(id);
@@ -51,10 +54,15 @@ var vm = new Vue({
     },
 
     submitVote: function (event) {
-/* TODO: 檢查是否已投過, 寫入DB */
-
+/*        if(! $('#voteConfirm').is(":checked")){
+            $('#voteMessage').html(vm.alert_2);
+            return;
+        };
+*/
         if( vm.vote.length != 3 ){
-            $('#voteMessage').html(vm.alert_1);
+            $('#voteMessage').html('<p><h4>每個帳號限投一次，每次須投滿三票不同作品<h4> \
+              <h5>您已投 ' + vm.vote.length + ' 票，還需投 ' + (3-vm.vote.length) + ' 票</h5><a onclick="parent.$.colorbox.close(); return false;" \
+              class="btn btn-warning btn-s" style="margin-left:20px">投票確認</a></h4></p>');
             return;
         };
 //        $('#voteMessage').html(vm.alert_final);
@@ -65,16 +73,10 @@ var vm = new Vue({
 
 });
 
-/*
-########
-網站上的 Facebook redirect URL 記得上線前要改回來
-#######
-*/
-
 
 $(document).ready(function(){
     // Color Box
-    $(".inline").colorbox({inline:true, width:"50%"});
+    $(".inline").colorbox({inline:true, width:"75%"});
 });
 
 $(document).ready(function(){
@@ -86,7 +88,7 @@ $(document).ready(function(){
         return;
     };
     if( thanks == 1){
-        alert('感謝您的參與，已完成投票。');
+        alert('感謝您的參與，已完成投票。視覺藝術獎、表演藝術獎、年度大獎 5/13 揭曉頒布。5/15 中午於本站公告抽獎中獎名單。');
         return;
     };
 
